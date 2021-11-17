24a41b67cd866cd195283798d550e593c7d3a7e2
rinnuagna <rinnuagna@gmail.com>
2021-11-17 20:15:58 +0530

    Removed commented lines

--- to-do-list-web/src/Menubar.js
+++ to-do-list-web/src/Menubar.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Menu, Header, Icon } from "semantic-ui-react";
 
 export default function Menubar() {
-  // const currentDate = new Date();
-  // const dateYr = `${currentDate.getDate()}/${currentDate.getFullYear()}`;
-  // let month = currentDate.toLocaleString('en-us', { month: 'long' });
   return (
     <div>
       <Menu borderless inverted color="blue" className="menubar-style">
@@ -14,9 +11,6 @@ export default function Menubar() {
             My To Do List
           </Header>
         </Menu.Item>
-        {/* <Menu.Menu position="right">
-          <Menu.Item>{dateYr}</Menu.Item>
-        </Menu.Menu> */}
       </Menu>
     </div>
   );
